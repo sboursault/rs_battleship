48bb133f7977172ce8db723a7fc78e6c2965fcc8
sboursault <sboursault@protonmail.com>
2022-06-12 11:35:23 +0200

    day 2: test on hit / destroyed / missed

--- src/battleship.rs
+++ src/battleship.rs
@@ -5,32 +5,40 @@ struct Board {
 
 impl Board {
     fn shoot(&mut self, coord: Coordinates) -> ShotResult {
-
-        for boat in &self.boats {
+        for boat in &mut self.boats {
             if boat.is_at(&coord) {
-                return ShotResult::Hit
+                return boat.hit(coord);
             }
         }
 
         ShotResult::Missed
     }
-
 }
 
 
 struct Boat {
-    position: Vec<Coordinates>
+    position: Vec<Coordinates>,
+    hits: Vec<Coordinates>,
 }
 
 impl Boat {
     fn new(position: Vec<Coordinates>) -> Boat {
-        return Boat { position };
+        return Boat { position, hits: vec![] };
     }
 
     fn is_at(&self, coord: &Coordinates) -> bool {
         self.position.contains(coord)
     }
 
+    fn hit(&mut self, coord: Coordinates) -> ShotResult {
+        self.hits.push(coord);
+        for boat_coord in &self.position {
+            if !self.hits.contains(boat_coord) {
+                return ShotResult::Hit;
+            }
+        }
+        return ShotResult::Destroyed
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -57,25 +65,18 @@ enum ShotResult {
 mod tests {
     use super::*;
 
-    #[test]
-    fn shoot_missed() {
-        let mut board = Board {
-            boats: vec![
-                Boat::new(vec![Coordinates::new(5, 2), Coordinates::new(6, 2)]),
-                Boat::new(vec![Coordinates::new(7, 3), Coordinates::new(7, 4), Coordinates::new(7, 5)])
-            ]
-        };
-        assert_eq!(board.shoot(Coordinates::new(2,2)), ShotResult::Missed);
-    }
 
     #[test]
-    fn shoot_hit() {
+    fn shoot() {
         let mut board = Board {
             boats: vec![
                 Boat::new(vec![Coordinates::new(5, 2), Coordinates::new(6, 2)]),
                 Boat::new(vec![Coordinates::new(7, 3), Coordinates::new(7, 4), Coordinates::new(7, 5)])
             ]
         };
-        assert_eq!(board.shoot(Coordinates::new(6,2)), ShotResult::Hit);
+        assert_eq!(board.shoot(Coordinates::new(5, 2)), ShotResult::Hit);
+        assert_eq!(board.shoot(Coordinates::new(4, 2)), ShotResult::Missed);
+        assert_eq!(board.shoot(Coordinates::new(7, 4)), ShotResult::Hit);
+        assert_eq!(board.shoot(Coordinates::new(6, 2)), ShotResult::Destroyed);
     }
 }
